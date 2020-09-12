72744a967c151f7ab4cb97c6bd9c07d79a83fe44
Faizan <faizanh53@gmail.com>
2020-09-12 18:40:23 +0100

    added ldr spio with front end chart

--- app.js
+++ app.js
@@ -9,7 +9,8 @@ const express = require('express')
 , server = require('http').createServer(app)
 , io = require('socket.io').listen(server)
 , ip = require('ip')
-, rpiDhtSensor = require('rpi-dht-sensor');
+, rpiDhtSensor = require('rpi-dht-sensor')
+, rpio = require('rpio');
 
 /* Database */
 const sqlite3 = require('sqlite3').verbose();
@@ -57,6 +58,43 @@ io.on('connection', function(socket) {
     console.log('client connected');
 
     setInterval(() => {
+//    	// Read values from SPI ch0
+//	rpio.spiBegin();
+//	// Prepare TV buffer
+//	var sendBuffer = new Buffer([0x01, (8 + 0 << 4), 0x01]);
+//	// Send TV buffer via MOSI and MISO
+//	var recieveBuffer = rpio.spiTransfer(sendBuffer, sendBuffer.length);
+//	
+//	// Extract val from output buffer
+//	var junk = recieveBuffer[0],
+//	    MSB = recieveBuffer[1],
+//	    LSB = recieveBuffer[2];
+//	
+//	// Ignore 6b of MSB, shift and combine
+//	var value = ((MSB & 3) << 8) + LSB;
+//	console.log('ch' + ((sendBuffer[1] >> 4) - 8), '=', value);
+
+rpio.spiBegin();
+
+// Prepare TX buffer [trigger byte = 0x01] [channel 0 = 0x80 (128)] [dummy data = 0x01]
+var txBuffer = new Buffer([0x01, (8 + 0 << 4), 0x01]); 
+
+var rxBuffer = new Buffer(8);
+// Send TX buffer to SPI MOSI and recieve RX buffer from MISO
+rpio.spiTransfer(txBuffer, rxBuffer, txBuffer.length);
+
+// Extract value from output buffer. Ignore first byte (junk). 
+var junk = rxBuffer[0],
+    MSB = rxBuffer[1],
+    LSB = rxBuffer[2];
+
+// Ignore first six bits of MSB, bit shift MSB 8 positions and 
+// lastly combine LSB with MSB to get a full 10 bit value
+var ldrVal = ((MSB & 3) << 8) + LSB; 
+
+console.log('ch' + ((txBuffer[1] >> 4) - 8), '=', ldrVal);
+
+    	// Read values from DHT sensor
         const dht = new rpiDhtSensor.DHT11(22);
         const readout = dht.read();
         const temp = (readout.temperature.toFixed(0));
@@ -65,5 +103,9 @@ io.on('connection', function(socket) {
         socket.emit('dht11', {
             temp, humid,
         });
+
+	socket.emit('ldr', {
+	    ldrVal,
+	});
     }, 3000);
 });
---
+++ public/js/ldr-socket.js
@@ -0,0 +1,20 @@
+(function(){
+  const socket = io.connect();
+
+  socket.on('ldr', function (data) {
+    console.log(data);
+    const d = new Date();
+    if (luminosityChart.data.datasets[0].data.length < 10) {
+      luminosityChart.data.datasets[0].data.push(data.ldrVal);
+      luminosityChart.data.labels.push(d.getMonth() + 1 + '/' + d.getDate() + '\r\n' + d.getHours() + ':' + d.getMinutes() + ':' + d.getSeconds());
+      luminosityChart.update();
+    } else {
+      luminosityChart.data.datasets[0].data.shift();
+      luminosityChart.data.labels.shift();
+      luminosityChart.data.datasets[0].data.push(data.ldrVal);
+      luminosityChart.data.labels.push(d.getMonth() + 1 + '/' + d.getDate() + '\r\n' + d.getHours() + ':' + d.getMinutes() + ':' + d.getSeconds());
+      luminosityChart.update();
+    }
+  });
+  
+})();
