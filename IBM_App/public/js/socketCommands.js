bb91c7d943a742eb03b353524bac5d9e81b95a50
Faizan <faizanh53@gmail.com>
2020-10-21 03:14:51 +1100

    Add files via upload

--- IBM_App/public/js/socketCommands.js
+++ IBM_App/public/js/socketCommands.js
@@ -1,17 +1,30 @@
 
 (function(){
-    const state = document.getElementById('state');
-    const deviceInfo = document.getElementById('deviceInfo');
+    // const deviceInfo = document.querySelectorAll('.deviceInfo');
     const eventData_temp = document.getElementById('deviceSensorData1');
     const eventData_ldr = document.getElementById('deviceSensorData2');
     const eventData_gps = document.getElementById('deviceSensorData3');
     const deviceslist = document.getElementById('deviceslist');
 
+    const id = document.querySelectorAll('.id');
+    const type = document.getElementById('type');
+    const gateway = document.getElementById('gateway');
+    const state = document.querySelectorAll('.state');
+
+    const ip = document.querySelectorAll('.ip');
+    const serial = document.getElementById('serial');
+    const manufacturer = document.getElementById('manufacturer');
+    const model = document.getElementById('model');
+    const description = document.getElementById('description');
+    const code = document.querySelectorAll('.sku');
+    const metadata = document.getElementById('metadata');
+
     const socket = io.connect();
-  
+
     socket.on('status', function (data) {
         state.innerHTML = data.state;
-        deviceInfo.innerHTML = data.deviceInfo;
+        // deviceInfo.innerHTML = data.deviceInfo;
+
     });
     socket.on('event', function (data) {
         eventData_temp.innerHTML = data.temp;
@@ -20,5 +33,30 @@
     });
     socket.on('devices', function(data) {
         deviceslist.innerHTML = data.deviceslist;
+    });
+    socket.on('device', function(data) {
+        id.forEach(function(el) {
+            el.innerHTML = data.id;
+        });
+        type.innerHTML = data.type;
+        gateway.innerHTML = data.gateway;
+        state.forEach(function(el5) {
+            el5.innerHTML = data.state;
+        });
+    });
+    socket.on('deviceInfoOnConnect', function(data) {
+        ip.forEach(function(el) {
+            el.innerHTML = data.ip;
+        });
+    });
+    socket.on('deviceInfoOnStandby', function(data) {
+        serial.innerHTML = data.serial;
+        manufacturer.innerHTML = data.manufacturer;
+        model.innerHTML = data.model;
+        description.innerHTML = data.description;
+        code.forEach(function(el) {
+            el.innerHTML = data.code;
+        });
+        metadata.innerHTML = data.metadata;
     });
 })();
